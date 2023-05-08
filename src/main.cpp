7c1640e86d9e5511ab378413569561cd1de0f96b
Matthieu Dreher <mdreher@greenorangutan.com>
2023-05-07 22:02:53 -0400

    Work in progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,52 @@
 #include <iostream>
+#include <imgui.h>
+#include <imgui_impl_sdl.h>
+
+#include <SDL.h>
 
 int main() {
 
     // Image
 
-    const int image_width = 256;
-    const int image_height = 256;
+    int width = 256;
+    int height = 256;
+
+    // SDL Context creation
+    SDL_Init( SDL_INIT_VIDEO );
+    SDL_GL_SetAttribute( SDL_GL_DOUBLEBUFFER, 1 );
+    SDL_GL_SetAttribute( SDL_GL_ACCELERATED_VISUAL, 1 );
+    SDL_GL_SetAttribute( SDL_GL_RED_SIZE, 8 );
+    SDL_GL_SetAttribute( SDL_GL_GREEN_SIZE, 8 );
+    SDL_GL_SetAttribute( SDL_GL_BLUE_SIZE, 8 );
+    SDL_GL_SetAttribute( SDL_GL_ALPHA_SIZE, 8 );
+
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 4 );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 6 );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE );
+
+    SDL_Window * window = SDL_CreateWindow( "", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height,  SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE );
+
+    SDL_GLContext context = SDL_GL_CreateContext( window );
+
+    SDL_GetWindowSize(window, &width, &height);
+
+    ImGui::CreateContext();
+    auto & io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    io.FontDefault = io.Fonts->AddFontFromFileTTF("/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf", 14);
+
+    ImGui_ImplSDL2_InitForOpenGL(window, context);
+
+    styleDark();
 
     // Render
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n" << width << ' ' << height << "\n255\n";
 
-    for (int j = image_height-1; j >= 0; --j) {
-        for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width-1);
-            auto g = double(j) / (image_height-1);
+    for (int j = height-1; j >= 0; --j) {
+        for (int i = 0; i < width; ++i) {
+            auto r = double(i) / (width-1);
+            auto g = double(j) / (height-1);
             auto b = 0.25;
 
             int ir = static_cast<int>(255.999 * r);
