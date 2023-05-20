1e235ff5cc58bbf727335711f8731a473ef458c8
Matthieu Dreher <mdreher@greenorangutan.com>
2023-05-20 13:55:49 -0400

    Switch from ray tracer to path tracer.

--- include/miquella/core/renderer.h
+++ include/miquella/core/renderer.h
@@ -8,6 +8,7 @@
 #include <miquella/core/utility.h>
 
 #include <numeric>
+#include <chrono>
 
 namespace miquella
 {
@@ -34,6 +35,7 @@ public:
 
 
         m_image.resize(static_cast<size_t>(m_width * m_height * 4));
+        m_imageAccumulated.resize(static_cast<size_t>(m_width * m_height));
     }
 
     void setScene(std::shared_ptr<Scene> scene){ m_scene = scene; }
@@ -72,43 +74,31 @@ public:
             return;
         }
 
-        int maxDepth = 50;
+        int maxDepth = 5;
 
-        for (int j = m_height-1; j >= 0; --j) {
-            for (int i = 0; i < m_width; ++i) {
+        auto startTime = std::chrono::steady_clock::now();
 
-                int ir = 0;
-                int ig = 0;
-                int ib = 0;
+        for (int j = m_height-1; j >= 0; --j)
+        {
+            for (int i = 0; i < m_width; ++i)
+            {
 
-                for(int k = 0; k < m_smaplePerPixel; k++)
-                {
-                    miquella::core::Ray ray = m_camera->generateRay(
-                                (static_cast<float>(i) + miquella::core::randomFloat()) / static_cast<float>(m_width - 1),
-                                (static_cast<float>(m_height - j - 1)  + miquella::core::randomFloat()) / static_cast<float>(m_height - 1)   // The camera (0,0) is bottom left, the texture is (0,0) is top left
-                                );
+                miquella::core::Ray ray = m_camera->generateRay(
+                            (static_cast<float>(i) + miquella::core::randomFloat()) / static_cast<float>(m_width - 1),
+                            (static_cast<float>(m_height - j - 1)  + miquella::core::randomFloat()) / static_cast<float>(m_height - 1)   // The camera (0,0) is bottom left, the texture is (0,0) is top left
+                            );
 
-                    /*miquella::core::hitRecord record;
-                    if(m_scene->intersect(ray, 0.1f, 10000000.0f, record))
-                    {
-                        // For now, we color with the normal of impact
-                        // The normalized normal components are between [-1, 1]
-                        // We are scaling them between [0, 1] to get the color
-                        auto normal = (glm::normalize(record.normal) + glm::vec3(1.0f, 1.0f, 1.0f)) / 2.0f;
-                        ir += static_cast<int>(254.999f * std::fabs(normal.x));
-                        ig += static_cast<int>(254.999f * std::fabs(normal.y));
-                        ib += static_cast<int>(254.999f * std::fabs(normal.z));
-                    }*/
-                    glm::vec3 color = processRay(ray, maxDepth);
-                    ir += static_cast<int>(254.999f * std::fabs(color.x));
-                    ig += static_cast<int>(254.999f * std::fabs(color.y));
-                    ib += static_cast<int>(254.999f * std::fabs(color.z));
-                }
+                glm::vec3 color = processRay(ray, maxDepth);
 
-                // Scaling down by the number of sample per pixel
-                ir = ir / m_smaplePerPixel;
-                ig = ig / m_smaplePerPixel;
-                ib = ib / m_smaplePerPixel;
+
+                auto indexAcc = static_cast<size_t>(j*m_width + i);
+                m_imageAccumulated[indexAcc] += color;
+
+                glm::vec3 pixelColor = m_imageAccumulated[indexAcc] / static_cast<float>(nbFrameAccumulated);
+
+                int ir = static_cast<int>(254.999f * std::fabs(pixelColor.x));
+                int ig = static_cast<int>(254.999f * std::fabs(pixelColor.y));
+                int ib = static_cast<int>(254.999f * std::fabs(pixelColor.z));
 
                 auto index = static_cast<size_t>(j*m_width*4 + i*4);
                 m_image[index] = static_cast<unsigned char>(ir);
@@ -117,6 +107,12 @@ public:
                 m_image[index+3] = static_cast<unsigned char>(255);
             }
         }
+
+        auto endTime = std::chrono::steady_clock::now();
+        m_executionTime = static_cast<size_t>(std::chrono::duration<double, std::milli>(endTime - startTime).count());
+        std::cout<<"Image computed in "<<m_executionTime<<" ms."<<std::endl;
+
+        nbFrameAccumulated++;
     }
 
     unsigned char* getImagePointer(){ return m_image.data(); }
@@ -126,10 +122,14 @@ public:
     std::shared_ptr<Camera> m_camera;
 
     std::vector<unsigned char> m_image;
+    std::vector<glm::vec3> m_imageAccumulated;
     int m_width = 0;
     int m_height = 0;
 
-    int m_smaplePerPixel = 100;
+    size_t m_executionTime = 0;
+
+    bool accumulate = true;
+    size_t nbFrameAccumulated = 1;
 };
 
 } // core
--- src/main.cpp
+++ src/main.cpp
@@ -129,9 +129,6 @@ int main() {
 
     renderer.setImageResolution(imageWidth, imageHeight);
 
-    // Compute the image
-    renderer.render();
-
     // Create a OpenGL texture identifier
     GLuint image_texture;
     glGenTextures(1, &image_texture);
@@ -142,13 +139,6 @@ int main() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // This is required on WebGL for non power-of-two textures
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // Same
-
-    // Upload pixels into texture
-#if defined(GL_UNPACK_ROW_LENGTH) && !defined(__EMSCRIPTEN__)
-    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
-#endif
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imageWidth, imageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, renderer.getImagePointer());
-
 
     // ---------------------- Event loop handling ----------------------------------
 
@@ -188,6 +178,16 @@ int main() {
                 }
             }
         }
+
+        // Compute the image
+        renderer.render();
+
+        // Upload pixels into texture
+        #if defined(GL_UNPACK_ROW_LENGTH) && !defined(__EMSCRIPTEN__)
+        glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+        #endif
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, imageWidth, imageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE, renderer.getImagePointer());
+
 
         // start the Dear ImGui frame
         ImGui_ImplOpenGL3_NewFrame();
